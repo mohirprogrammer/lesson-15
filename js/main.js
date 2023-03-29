4d9b2e9f08cc4e1c1d65ced9591bbd129959767a
mohirprogrammer <mohirprogrammer@gmail.com>
2023-03-29 23:21:56 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+// aside
+
+const sidebar = document.querySelector("aside");
+const sidebarToggle = document.querySelector(".hamburger");
+
+sidebarToggle.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+// dark-light
+
+const body = document.body;
+const modeToggle = document.querySelector(".switch");
+
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
